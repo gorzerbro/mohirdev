7f79428d74039ae133b1dc89fd56f6c06c18fdc2
gorzerbro <brogorzer@gmail.com>
2025-03-20 19:19:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Radiusni kiriting: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        double S = Math.PI * Math.Pow(radius, 2);
+        double L = 2 * Math.PI * radius;
+
+        Console.WriteLine($"S = {S}, L = {L}");
+    }
+}
